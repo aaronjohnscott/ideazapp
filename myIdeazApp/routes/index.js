259c8355ec8b320e676aacbc8a21aafb5957956e
Aaron Scott <aaronjohnscott@gmail.com>
2019-07-14 15:03:51 -0700

    Username
    Added username to home screen using the parameters

--- myIdeazApp/routes/index.js
+++ myIdeazApp/routes/index.js
@@ -7,29 +7,24 @@ router.get("/", function (req, res, next) {
   res.render("index", {});
 });
 
-router.get("/home", function (req, res, next) {
-  res.render("home");
+router.get("/home/:username", function (req, res, next) {
+  let { username } = req.params
+  res.render("home", { username });
 });
 
 
 router.post("/addUser", (req, res) => {
-
   let { username, email, password } = req.body;
-
-
   User.create({
     username,
     password,
     email
   })
     .then(() => {
-      res.redirect("/home")
+      res.redirect(`/home/${username}`)
     })
     .catch(e => console.log(e));
 });
-
-
-
 
 module.exports = router;
 
